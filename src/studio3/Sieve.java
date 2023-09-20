032d4fc450fc62965ea68c4e0ed766bb8453a90c
james <james@localhost>
2023-09-20 15:46:17 -0500

    Attempted to make a Sieve

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,34 @@
+package studio3;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		
+		//int[] array	;
+		//array = new int[40]	;
+		int[] array = new int[40]	;
+		int p	;
+		
+		for (int i=2; i<=array.length; i++ )
+		{
+			System.out.println(i);
+			
+			
+			
+			for (int a = (2 * i); a<=array.length; a++)	
+			{
+				System.out.println(a);
+			}
+			
+			
+			
+		}
+				
+		
+		
+		
+		
+
+	}
+
+}
